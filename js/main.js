77d62a36592e1e1245dbbb8800b3ab644f031318
TuzovDi <tuzovjw@gmail.com>
2022-10-15 14:42:04 +0300

    Connected JS script.
    made work on mistakes
    
    Created Two Functions: getRandomInt , checkLength

--- js/main.js
+++ js/main.js
@@ -6,22 +6,22 @@
 // Взято с сайта:
 // https://schoolsw3.com/js/js_random.php
 
- function getRandomInt(min, max) {
+function getRandomInt(min, max) {
   if (min < 0 || min >= max) {
-      return NaN
+    return NaN;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-getRandomInt(0,100);
+getRandomInt(0, 100);
 
 // Функция №2
 // Прверка длины комментария
 
-const line = 'Lets check function!'
+const checkline = 'Lets check function!';
 function checkLength(line, maxLength) {
   if (line.length > maxLength) {
     return false;
   }
-    return true;
+  return true;
 }
-checkLength(line,15);
+checkLength(checkline, 15);
